2d3274cd65a7f334368246766eaa0a319aae7322
Lakshmidevi007 <velmuruganseeman5@gmail.com>
2026-03-10 16:17:14 +0530

    update backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+
+  // CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.write("Hello from backend server!");
   res.end();
